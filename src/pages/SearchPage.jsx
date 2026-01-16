471c0aed4e2725d3a52e5d6a245d8c4ae44aba49
Ramon Santos <ramonpps@id.uff.br>
2026-01-15 21:09:08 -0300

    debug

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Navbar from '../Navbar_temp';
+import Navbar from '../Navbar';
 
 const SearchPage = () => {
   const [query, setQuery] = useState('');
